2f5c53652195fa8f4d50db0068ed7d60a4001020
Tiogiras <tiogiras@gmail.com>
2024-01-11 00:05:12 +0100

    Initialized Package

---
+++ Editor/Scripts/DisplayContent.cs
@@ -0,0 +1,48 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.UIElements;
+
+namespace Editor.Scripts
+{
+
+internal static partial class DisplayContent
+{
+    private const string BasePathAlphaButton = "Alpha Button/User Interface/Display Content Tabs/";
+
+    #region Private Methods
+
+    // Called by reflection
+    // ReSharper disable once UnusedMember.Local
+    private static void AlphaButton(VisualElement root)
+    {
+        var tabs = root.Q <GroupBox>("Tabs");
+        var tabContentParent = root.Q <GroupBox>("TabContent");
+
+        RegisterTabCallbacks(tabs, tabContentParent, 1);
+
+        SetTabContentLocations(BasePathAlphaButton + "Tab0");
+
+        s_onSelectedTabChanged += OnTabChangedAlphaButton;
+        s_onSelectedPackageChanged += UnsubscribeAlphaButton;
+
+        SwitchTab(tabContentParent, 0);
+    }
+
+    private static void OnTabChangedAlphaButton(int tab, VisualElement root)
+    {
+        switch (tab)
+        {
+
+        }
+    }
+
+    private static void UnsubscribeAlphaButton()
+    {
+        s_onSelectedTabChanged -= OnTabChangedAlphaButton;
+        s_onSelectedPackageChanged -= UnsubscribeAlphaButton;
+    }
+
+    #endregion
+}
+
+}
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MegaPint-AlphaButton
+Package for the MegaPint package collection that adds an button component that uses the build in alphatransparency.
+
+All further information can be found after installing the package via MegaPint.
