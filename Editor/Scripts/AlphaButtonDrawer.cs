5769afb4a408ef11facb124d82959ce3f75011dd
Tiogiras <tiogiras@gmail.com>
2024-01-18 17:47:50 +0100

    Added base functionality

---
+++ Editor/Scripts/AlphaButtonDrawer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace Editor.Scripts
+{
+
+[CustomEditor(typeof(AlphaButton))]
+public class AlphaButtonDrawer : ButtonEditor
+{
+    #region Public Methods
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("Alpha Settings", EditorStyles.boldLabel);
+
+        SerializedProperty threshold = serializedObject.FindProperty("_alphaThreshold");
+        EditorGUILayout.PropertyField(threshold);
+
+        serializedObject.ApplyModifiedProperties();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Button Settings", EditorStyles.boldLabel);
+
+        base.OnInspectorGUI();
+    }
+
+    #endregion
+}
+
+}
---
+++ Runtime/AlphaButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphaButton : Button
+{
+    [Tooltip("Alpha threshold for interacting with the button")]
+    [SerializeField] [Range(0f, 1f)] private float _alphaThreshold = .5f;
+
+    #region Unity Event Functions
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (targetGraphic is not Image castedImage)
+            return;
+
+        castedImage.alphaHitTestMinimumThreshold = _alphaThreshold;
+    }
+
+    #endregion
+}
