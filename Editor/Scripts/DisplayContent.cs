eac969733b20a682afacea72efd1b8830ca1b222
Tiogiras <tiogiras@gmail.com>
2024-04-17 18:58:28 +0200

    GUI Rework

--- Editor/Scripts/DisplayContent.cs
+++ Editor/Scripts/DisplayContent.cs
@@ -1,5 +1,9 @@
 ﻿#if UNITY_EDITOR
+using Editor.Scripts.PackageManager.Cache;
+using Editor.Scripts.PackageManager.Packages;
+using Editor.Scripts.Windows;
 using UnityEngine.UIElements;
+using GUIUtility = Editor.Scripts.GUI.GUIUtility;
 
 namespace Editor.Scripts
 {
@@ -10,8 +14,24 @@ internal static partial class DisplayContent
 
     // Called by reflection
     // ReSharper disable once UnusedMember.Local
-    // ReSharper disable once UnusedParameter.Local
-    private static void AlphaButton(VisualElement _) { }
+    private static void AlphaButton(DisplayContentReferences refs)
+    {
+        VisualElement root = InitializeDisplayContent(refs);
+
+        root.Q <Label>("PackageInfo").text = PackageCache.Get(PackageKey.AlphaButton).Description;
+        
+        GUIUtility.ActivateLinks(root,
+                                 evt =>
+                                 {
+                                     switch (evt.linkID)
+                                     {
+                                         case "integration":
+                                             MegaPintPackageManagerWindow.OpenPerLink(PackageKey.AlphaButton);
+                                             break;
+                                     }
+                                 });
+    }
+    
 
     #endregion
 }
